0845698613d0067d5b822554e4ff4d0ff7266ce7
pavan <balubadi.pavankumar@gmail.com>
2024-07-06 16:59:42 +0530

    tf-module-vpc

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 module "vpc" {
-  source = "git::https://github.com/balubadi/tf-module-vpc1.git"
-  env    = var.env
-  tags   = var.tags
-  vpc_cidr_block = var.vpc_cidr_block
-  app_subnets = var.public_subnets
-  azs = var.azs
-  db_subnets = var.db_subnets
-  public_subnets = var.public_subnets
-  web_subnets = var.web_subnets
-  account_id = var.account_id
-  default_route_table_id = var.default_route_table_id
-  default_vpc_id = var.default_vpc_id
-}
+  source = "git::https://github.com/raghudevopsb76/tf-module-vpc.git"
 
+  for_each       = var.vpc
+  vpc_cidr_block = each.value["vpc_cidr_block"]
+  public_subnets = each.value["public_subnets"]
+  web_subnets    = each.value["web_subnets"]
+  app_subnets    = each.value["app_subnets"]
+  db_subnets     = each.value["db_subnets"]
+  azs            = each.value["azs"]
+
+  env                    = var.env
+  tags                   = var.tags
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_vpc_id         = var.default_vpc_id
+  account_id             = var.account_id
+  default_route_table_id = var.default_route_table_id
+}
