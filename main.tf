b3d0064d6167b9c6d315d9994174988b3becda7d
pavan <balubadi.pavankumar@gmail.com>
2024-07-06 17:53:55 +0530

    kms

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/raghudevopsb76/tf-module-vpc.git"
+  source = "git::https://github.com/balubadi/tf-module-vpc1.git"
 
   for_each       = var.vpc
   vpc_cidr_block = each.value["vpc_cidr_block"]
