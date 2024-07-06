9c3a12bce80da28c447d3ceb96098ffab9db6738
pavan <balubadi.pavankumar@gmail.com>
2024-07-06 18:21:14 +0530

    terraform eks

--- vars.tf
+++ vars.tf
@@ -5,7 +5,6 @@ variable "default_vpc_id" {}
 variable "default_route_table_id" {}
 variable "default_vpc_cidr" {}
 variable "vpc" {}
-variable "rds" {}
 variable "route53_zone_id" {}
 variable "kms" {}
 variable "eks" {}
