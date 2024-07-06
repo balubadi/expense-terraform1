716cf96238eb4f10bc32a56bcdb18af2d75a53e5
pavan <balubadi.pavankumar@gmail.com>
2024-07-06 16:38:54 +0530

    expense-terraform1

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
-
 module "vpc" {
-  source                 = "git::https://github.com/balubadi/tf-module-vpc1.git"
-  vpc_cidr_block         = var.vpc_cidr_block
-  env                    = var.env
-  tags                   = var.tags
-  public_subnets         = var.public_subnets
-  web_subnets            = var.web_subnets
-  app_subnets            = var.app_subnets
-  db_subnets             = var.db_subnets
-  azs                    = var.azs
-  account_id             = var.account_id
-  default_vpc_id         = var.default_vpc_id
+  source = "git::https://github.com/balubadi/tf-module-vpc1.git"
+  env    = var.env
+  tags   = var.tags
+  vpc_cidr_block = var.vpc_cidr_block
+  app_subnets = var.public_subnets
+  azs = var.azs
+  db_subnets = var.db_subnets
+  public_subnets = var.public_subnets
+  web_subnets = var.web_subnets
+  account_id = var.account_id
   default_route_table_id = var.default_route_table_id
 }
 
